1db13c64df49ae113d1630f96bc86c6af67da7a1
Varun Raj <145306368+Thunder846@users.noreply.github.com>
2026-09-07 21:26:02 +0530

    Create CTAS.sql

---
+++ CTAS.sql
@@ -0,0 +1,18 @@
+-- CTAS
+-- Task 6: Create Summary Tables: Used CTAS to generate new tables based on query results - each book and total book_issued_cnt**
+
+CREATE TABLE book_cnts
+AS    
+SELECT 
+    b.isbn,
+    b.book_title,
+    COUNT(ist.issued_id) as no_issued
+FROM books as b
+JOIN
+issued_status as ist
+ON ist.issued_book_isbn = b.isbn
+GROUP BY 1, 2;
+
+
+SELECT * FROM
+book_cnts;
